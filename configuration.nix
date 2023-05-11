b060980b3e37fcc8a432f48c182a44394c31ed73
Valentin Rieß <valentin.riess@posteo.de>
2023-05-11 23:12:44 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,8 @@
-{ config, pkgs, lib, ... }:
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
 
 let
   unstableTarball =
@@ -6,38 +10,109 @@ let
       https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
 in
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/virtualbox-demo.nix>
-    ./home.nix
-  ];
-  
-  # Let demo build as a trusted user.
-  # nix.settings.trusted-users = [ "demo" ];
-  
-  # Mount a VirtualBox shared folder.
-  # This is configurable in the VirtualBox menu at
-  # Machine / Settings / Shared Folders.
-  # fileSystems."/mnt" = {
-  #   fsType = "vboxsf";
-  #   device = "nameofdevicetomount";
-  #   options = [ "rw" ];
-  # };
-  
-  # By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
-  # If you prefer another desktop manager or display manager, you may want
-  # to disable the default.
-  services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
-  services.xserver.displayManager.sddm.enable = lib.mkForce false;
-  
-  # Enable GDM/GNOME by uncommenting above two lines and two lines below.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-  
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ./home.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # Setup keyfile
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+  # Enable swap on luks
+  boot.initrd.luks.devices."luks-1dbda026-dd09-4abc-8a83-f0a5591074a3".device = "/dev/disk/by-uuid/1dbda026-dd09-4abc-8a83-f0a5591074a3";
+  boot.initrd.luks.devices."luks-1dbda026-dd09-4abc-8a83-f0a5591074a3".keyFile = "/crypto_keyfile.bin";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
-  
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "de-latin1-nodeadkeys";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.vriess = {
+    isNormalUser = true;
+    description = "Valentin RIess";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+    #  thunderbird
+    ];
+  };
+
+  # Allow unfree packages
+  # nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
-  # \$ nix search wget
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
     wget
     curl
@@ -50,18 +125,6 @@ in
     neofetch
     rsync
   ];
-  
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-  
-  # Enable nix-command
-  nix.settings.experimental-features = [ "nix-command" ];
-
-  # Set german keyboard layout
-  services.xserver = {
-    layout = "de";
-    xkbVariant = "nodeadkeys,";
-  };
 
   nixpkgs.config = {
     # Enable unstable channel as rolling release
@@ -73,9 +136,6 @@ in
     # Enable unfree packages for vscode
     allowUnfree = true;
   };
-
-  # Use X11 instead of wayland (needed for virtualbox)
-  services.xserver.displayManager.gdm.wayland = false;
 
   # GNOME Extension for systray icons
   services.udev.packages = with pkgs; [ gnome.gnome-settings-daemon ];
@@ -91,4 +151,32 @@ in
   # Set default shell to zsh
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
 }
